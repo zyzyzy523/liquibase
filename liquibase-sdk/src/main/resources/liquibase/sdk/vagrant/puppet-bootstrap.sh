48e1e9a06d0e17f397ae442e0750852c2eebfe29
Nathan Voxland <nathan@voxland.net>
2013-11-12 16:08:01 -0600

    Cleaner puppet-bootstrap.sh output

--- liquibase-sdk/src/main/resources/liquibase/sdk/vagrant/puppet-bootstrap.sh
+++ liquibase-sdk/src/main/resources/liquibase/sdk/vagrant/puppet-bootstrap.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-echo "Installing librarian-puppet"
-gem install librarian-puppet --no-rdoc --no-ri
+if ! gem list librarian-puppet -i > /dev/null 2>&1; then
+    echo "Installing librarian-puppet"
+    gem install librarian-puppet --no-rdoc --no-ri
+fi
 
 echo "Running librarian-puppet install"
-mkdir /etc/puppet
+mkdir -p /etc/puppet
 cd /etc/puppet
 cp /vagrant/Puppetfile .
 librarian-puppet install
 
 echo 'Copying vagrant-install-files...'
-mkdir /install
+mkdir -p /install
 cp -rn /vagrant-install-files/* /install
 
 echo "puppet-boostrap.sh done"
