b30cb93b986d8c3626b3979d9c387774b48bfb30
Nathan Voxland <nathan@voxland.net>
2019-03-22 00:14:56 -0500

    Imported https://github.com/liquibase/liquibase-teradata

---
+++ liquibase-teradata/src/main/java/liquibase/ext/teradata/datatype/BooleanType.java
@@ -0,0 +1,28 @@
+package liquibase.ext.teradata.datatype;
+
+import liquibase.database.Database;
+import liquibase.datatype.DatabaseDataType;
+import liquibase.ext.teradata.database.TeradataDatabase;
+
+public class BooleanType extends liquibase.datatype.core.BooleanType {
+
+    @Override
+    public boolean supports(Database database) {
+        return database instanceof TeradataDatabase;
+    }
+
+    @Override
+    public int getPriority() {
+        return PRIORITY_DATABASE;
+    }
+
+    @Override
+    public DatabaseDataType toDatabaseDataType(Database database) {
+        return new DatabaseDataType("BYTEINT");
+    }
+
+    @Override
+    protected boolean isNumericBoolean(Database database) {
+        return true;
+    }
+}
